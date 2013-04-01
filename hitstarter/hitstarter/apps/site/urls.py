a1bcb737eab7ad90a5203cc1a485c8a3b368f4a1
Rich Jones <rich@anomos.info>
2013-04-01 01:38:36 -0700

    imgs

--- hitstarter/hitstarter/apps/site/urls.py
+++ hitstarter/hitstarter/apps/site/urls.py
@@ -4,4 +4,5 @@ from hitstarter.apps.site import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^p/(?P<project_id>\d+)/.*?$', 'hitstarter.apps.site.views.project'),
+    
 )
--- hitstarter/hitstarter/urls.py
+++ hitstarter/hitstarter/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,4 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^', include('hitstarter.apps.site.urls', namespace='site')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 )
