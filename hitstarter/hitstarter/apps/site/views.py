63b77aed7acbf40fcf23eaf7e898c804a8dd5543
Rich Jones <rich@anomos.info>
2013-04-07 11:46:07 -0700

    derp

--- hitstarter/hitstarter/apps/site/views.py
+++ hitstarter/hitstarter/apps/site/views.py
@@ -50,7 +50,7 @@ def project(request, project_id):
 
 
 def is_onion(request):
-    if 'htstrtc3uttwk4li.onion' == request.META['HTTP_HOST'][:22]:
+    if 'htstrtc3uttwk4li.onion' in request.META['HTTP_HOST'][:30]:
         return True
     else:
         return False
